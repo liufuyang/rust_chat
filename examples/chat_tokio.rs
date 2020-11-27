ed43847df89def16f4c45f2f490bf030cb7636a3
Fuyang Liu <liufuyang@users.noreply.github.com>
2020-11-27 14:44:43 +0100

    let client do read

--- examples/chat_tokio.rs
+++ examples/chat_tokio.rs
@@ -1,15 +1,18 @@
 use tokio::net::TcpStream;
 use tokio::prelude::*;
 use std::error::Error;
-use std::net::Shutdown;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // Connect to a peer
     let mut stream = TcpStream::connect("127.0.0.1:3333").await?;
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
 
     // Write some data.
     stream.write_all(b"hello world!\n").await?;
-    // stream.shutdown(Shutdown::Both).expect("Shutdown error");
+    stream.read(&mut data).await?;
+    println!("data read: [{}]", std::str::from_utf8(&data).unwrap());
+    // Note, without reading from server and exit here will cause a "Connection reset by peer" error on server while server's trying to read again
+
     Ok(())
 }
